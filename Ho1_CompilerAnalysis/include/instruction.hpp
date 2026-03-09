35f621d8996513c5d3decf259b7366f7779fb73d
hash112 <hash.cpp@proton.me>
2026-03-09 01:33:36 -0600

    cambio en la tarea

---
+++ Ho1_CompilerAnalysis/include/instruction.hpp
@@ -0,0 +1,17 @@
+#ifndef INSTRUCCION_H
+#define INSTRUCCION_H
+
+class instruction
+{
+private:
+    /* data */
+public:
+    instruction(/* args */);
+};
+
+instruction::instruction(/* args */)
+{
+}
+
+
+#endif
